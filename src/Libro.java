6c9f30c31ed37133d39ee929910fd7d630ade83d
TLocatelli <thiagolocatelli12@gmail.com>
2026-07-18 02:29:25 -0300

    Agrege nuevos metodos en la clase Libro(estaPrestado, prestar, devolver), y el atributo prestado

--- src/Libro.java
+++ src/Libro.java
@@ -2,16 +2,27 @@ public class Libro{
     private int id;
     private String titulo;
     private String autor;
+    private boolean prestado;
 
     //Constructor
     public Libro(int id, String titulo, String autor) {
         this.id = id;
         this.titulo = titulo;
         this.autor = autor;
+        prestado = false;
     }
     //Metodos
     public void mostrarInformacion() {
         System.out.printf("Id: %d | Titulo: %s | Autor: %s%n", id, titulo, autor);
+    }
+    public boolean estaPrestado() {
+        return prestado;
+    }
+    public void prestar() {
+        prestado = true;
+    }
+    public void devolver() {
+        prestado = false;
     }
     //Getters
     public int getId() {
